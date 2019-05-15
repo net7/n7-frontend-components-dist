182314ccd83e56503147648c48e87dfdb0b4cc64
Edgar Gomez <gomez@netseven.it>
2019-05-15 10:57:54 +0200

    dist updated to version v0.0.04

---
+++ esm5/n7-frontend-components.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { DvComponentsLibModule, TestComponent, TitleComponent } from './public-api';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibjctZnJvbnRlbmQtY29tcG9uZW50cy5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0BuNy1mcm9udGVuZC9jb21wb25lbnRzLyIsInNvdXJjZXMiOlsibjctZnJvbnRlbmQtY29tcG9uZW50cy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEscUVBQWMsY0FBYyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL3B1YmxpYy1hcGknO1xuIl19
---
+++ fesm5/n7-frontend-components.js
@@ -0,0 +1,129 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, NgModule } from '@angular/core';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * TestComponent <dv-test-component>
+ *
+ * example:
+ * ```html
+ * <dv-test-component [data]="{
+ *   'id': 'test-component-id',
+ *   'title': 'test component title',
+ *   'body': 'test body...'
+ * }"></dv-test-component>
+ * ```
+ */
+var TestComponent = /** @class */ (function () {
+    function TestComponent() {
+    }
+    /**
+     * click event
+     *
+     * @param data
+     * @returns
+     * @memberof TestComponent
+     */
+    /**
+     * click event
+     *
+     * \@memberof TestComponent
+     * @param {?} data
+     * @return {?}
+     */
+    TestComponent.prototype.onClick = /**
+     * click event
+     *
+     * \@memberof TestComponent
+     * @param {?} data
+     * @return {?}
+     */
+    function (data) {
+        if (!this.emit)
+            return;
+        this.emit('click', data);
+    };
+    TestComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'dv-test-component',
+                    template: "<div *ngIf=\"data\" class=\"dv-component__test\" (click)=\"onClick(data)\">\n  <h2><small>({{ data.id }})</small> {{ data.title }}</h2>\n  <p>{{ data.body }}</p>\n</div>",
+                    styles: [".dv-component__test{background-color:#eee}"]
+                }] }
+    ];
+    TestComponent.propDecorators = {
+        data: [{ type: Input }],
+        emit: [{ type: Input }]
+    };
+    return TestComponent;
+}());
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * TitleComponent <dv-title-component>
+ *
+ * example:
+ * ```html
+ * <dv-title-component [data]="{
+ *   'label': 'title component label',
+ *   'value': 123
+ * }"></dv-title-component>
+ * ```
+ */
+var TitleComponent = /** @class */ (function () {
+    function TitleComponent() {
+    }
+    TitleComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'dv-title-component',
+                    template: "<h1 *ngIf=\"data\">{{ data.label }} ({{ data.value }})</h1>",
+                    styles: ["h1{color:red}"]
+                }] }
+    ];
+    TitleComponent.propDecorators = {
+        data: [{ type: Input }]
+    };
+    return TitleComponent;
+}());
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/** @type {?} */
+var COMPONENTS = [
+    TestComponent,
+    TitleComponent
+];
+var DvComponentsLibModule = /** @class */ (function () {
+    function DvComponentsLibModule() {
+    }
+    DvComponentsLibModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: COMPONENTS,
+                    entryComponents: COMPONENTS,
+                    imports: [CommonModule],
+                    exports: COMPONENTS,
+                },] }
+    ];
+    return DvComponentsLibModule;
+}());
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+export { DvComponentsLibModule, TestComponent, TitleComponent };
+
+//# sourceMappingURL=n7-frontend-components.js.map
